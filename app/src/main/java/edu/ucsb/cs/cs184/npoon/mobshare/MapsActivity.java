f20a1aebc1f8b64c352cae1d404ee2bd3b14886b
juliomaldonado <juliomaldonadov1@gmail.com>
2017-11-29 23:48:25 -0800

    Testing push for Julio.

--- app/src/main/java/edu/ucsb/cs/cs184/npoon/mobshare/MapsActivity.java
+++ app/src/main/java/edu/ucsb/cs/cs184/npoon/mobshare/MapsActivity.java
@@ -43,7 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-        //Julio can push to github!
+        //Julio can push to github! Test.
 
     }
 }
