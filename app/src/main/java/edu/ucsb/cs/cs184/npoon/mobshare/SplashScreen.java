cff51ee0a666f66a512498936f54d02e1e7f4e77
nickkpoon <nickkpoon@gmail.com>
2017-12-02 17:03:18 -0800

    splash screen live

---
+++ app/src/main/java/edu/ucsb/cs/cs184/npoon/mobshare/SplashScreen.java
@@ -0,0 +1,31 @@
+package edu.ucsb.cs.cs184.npoon.mobshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by nickkpoon on 12/2/17.
+ */
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MapsActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 2000);
+    }
+}
