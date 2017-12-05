cf89dde43e5208d4368a17358c5ef6830553a5eb
nickkpoon <nickkpoon@gmail.com>
2017-12-04 21:51:49 -0800

    welcome/login/signup screens completed

---
+++ app/src/main/java/edu/ucsb/cs/cs184/npoon/mobshare/WelcomeScreen.java
@@ -0,0 +1,53 @@
+package edu.ucsb.cs.cs184.npoon.mobshare;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by nickkpoon on 12/4/17.
+ */
+
+public class WelcomeScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome_screen);
+
+        Button signinButton = (Button) findViewById(R.id.welcome_signin_button);
+        Button registerButton = (Button) findViewById(R.id.welcome_register_button);
+
+        signinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(WelcomeScreen.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(WelcomeScreen.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        /*final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(WelcomeScreen.this, LoginActivity.class);
+                startActivity(intent);
+                overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                finish();
+            }
+        }, 2000);*/
+    }
+}
